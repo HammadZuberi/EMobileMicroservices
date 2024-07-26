ceb422b2aa852f60da50dae722a11a54fe49aa25
HammadZuberi <hammad.zuberi@hrsgbpo.com>
2024-07-25 20:14:53 -0400

    intial commit with Docker compose for Catalog Api

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,40 @@
+﻿
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string Name,
+        List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+
+    //using primary ctor and directly as marten is already absttract 
+
+    //internal class CreateProductCmdHandler
+    internal class CreateProductCmdHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>  
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //buissness logig to create a product
+            //TODO
+            //save to Db
+            //return result
+
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            session.Store(product);
+          await  session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.ID);
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//ADd services
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+var app = builder.Build();
+//configure http request pipeline
+
+app.MapCarter();
+app.Run();
